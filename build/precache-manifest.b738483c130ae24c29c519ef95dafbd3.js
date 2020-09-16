ea30cd5a6f50b859492eccb1b05dd4f39775ef77
dhiraj-deb-au7 <dhirajdeb@hotmail.com>
2020-09-16 18:56:43 +0530

    m-tube for netlify

---
+++ build/precache-manifest.b738483c130ae24c29c519ef95dafbd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab4f5588d7a39ec5455d5108699b996",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b276d96aa0b52417fdc",
+    "url": "/static/css/main.abd58810.chunk.css"
+  },
+  {
+    "revision": "cd4641ab908f1746ef41",
+    "url": "/static/js/2.3e591748.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3e591748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b276d96aa0b52417fdc",
+    "url": "/static/js/main.b09d47f0.chunk.js"
+  },
+  {
+    "revision": "6a040985a61f21896475",
+    "url": "/static/js/runtime-main.8ccca7a7.js"
+  },
+  {
+    "revision": "37194ebb0d43ab3274bf6760a7e95262",
+    "url": "/static/media/Loading-Image-1.37194ebb.gif"
+  }
+]);
